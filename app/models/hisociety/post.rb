7fa40620fd19f000cc8fcab746f6fc1eef63e7cc
Jonathan Twaddell <jon@arclocal.com>
2013-05-02 14:06:55 -0700

    test to see if this works

--- app/models/hisociety/post.rb
+++ app/models/hisociety/post.rb
@@ -2,5 +2,9 @@ module Hisociety
   class Post < ActiveRecord::Base
     attr_accessible :body, :header, :user_id
     belongs_to :user
+
+    def self.say_hello
+      puts "hello: " + Object.const_get(User).to_s
+    end
   end
 end
