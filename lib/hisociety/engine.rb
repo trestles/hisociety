ea441c726fb5f6d22f624fcebf5c999c50a6c89f
Jonathan Twaddell <jon@arclocal.com>
2013-05-02 14:01:29 -0700

    updated

--- lib/hisociety/engine.rb
+++ lib/hisociety/engine.rb
@@ -1,5 +1,8 @@
 module Hisociety
   class Engine < ::Rails::Engine
     isolate_namespace Hisociety
+    #config.to_prepare do
+    Decorators.register! Engine.root, Rails.root
+    #end
   end
 end
