afeb15bb41634fa55e544f90ae25dc2a42a37a33
Jonathan Twaddell <jon@arclocal.com>
2013-05-02 14:26:59 -0700

    take out decorators

--- lib/hisociety/engine.rb
+++ lib/hisociety/engine.rb
@@ -2,7 +2,7 @@ module Hisociety
   class Engine < ::Rails::Engine
     isolate_namespace Hisociety
     #config.to_prepare do
-    Decorators.register! Engine.root, Rails.root
+    #Decorators.register! Engine.root, Rails.root
     #end
   end
 end
